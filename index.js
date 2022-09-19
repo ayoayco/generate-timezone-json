48784f16f98a3ecbf9a8cac1bcc2a0d04a484756
Ayo <ramon.aycojr@gmail.com>
2022-09-19 15:26:24 +0200

    feat: sort the data

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
+const fs = require("fs");
 
-const fs = require('fs');
-
-console.log('Generating Timezone JSON');
+console.log("Generating Timezone JSON");
 
 const args = process.argv;
 
 if (args.length < 3) {
     // no file
-    const tzdb = require('@vvo/tzdb');
-    console.log('... from IANA Database');
-    const data = tzdb.getTimeZones({includeUtc: true});
-    fs.writeFile('timezones.json', JSON.stringify(data), (err) => {
-        if (err) throw err;
-        console.log('... done writing to file: timezones.json');
-    });
+    const tzdb = require("@vvo/tzdb");
+    console.log("... from IANA Database");
+    const data = JSON.stringify(tzdb.getTimeZones({ includeUtc: true }).sort(compare));
+    writeToFile(data);
 } else {
-    const fileName = args[2]
-    console.log('... from file', fileName);
+    // file name
+    const fileName = args[2];
+    const parseZonetab = require("parse-zonetab");
+
+    console.log("... from file", fileName);
+
+    fs.readFile(fileName, (err, raw) => {
+
+        if (err) throw err;
+
+        parseZonetab(raw).then(rows => {
+            const data = JSON.stringify(rows.sort(compare));
+            writeToFile(data);
+        });
+
+
+    });
 }
+
+function writeToFile(data) {
+
+    fs.writeFile("timezones.json", data, (err) => {
+        if (err) throw err;
+        console.log("... done writing to file: timezones.json");
+    });
+}
+
+function compare(a, b) {
+    const bid = b.id || b.name;
+    const aid = a.id || a.name;
+
+    if (aid < bid) {
+        return -1;
+    }
+    if (aid > bid) {
+        return 1;
+    }
+    return 0;
+};
+
